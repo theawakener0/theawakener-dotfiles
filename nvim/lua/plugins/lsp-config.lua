cd8fca272c0d1cb85a5f872b2a5db25a8cda5fd6
theawakener0 <wakeupwithme000@gmail.com>
2025-08-20 20:00:09 +0300

    nvimrc config

---
+++ nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,39 @@
+return {
+    {
+        "mason-org/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end
+    },
+    {
+        "mason-org/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "lua_ls",
+                    "ts_ls",
+                    "clangd",
+                    "pyright",
+                    "gopls"
+                }
+            })
+        end
+    },
+    {
+        "https://github.com/neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+            lspconfig.lua_ls.setup({capabilities = capabilities})
+            lspconfig.ts_ls.setup({capabilities = capabilities})
+            lspconfig.clangd.setup({capabilities = capabilities})
+            lspconfig.pyright.setup({capabilities = capabilities})
+            lspconfig.gopls.setup({capabilities = capabilities})
+            vim.keymap.set('n', "K", vim.lsp.buf.hover, {})
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+            vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
+        end
+    }
+
+}
+
---
+++ nvim/lua/vim-opt.lua
@@ -0,0 +1,10 @@
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+vim.opt.relativenumber = true
+
+
